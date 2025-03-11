e232d011103b09fc24c7246302594c136a9df8d8
saran <saransathi1010@gmail.com>
2025-03-11 14:46:25 +0530

    req

--- Website/src/utils/api.js
+++ Website/src/utils/api.js
@@ -34,7 +34,7 @@
 // api.ts
 import axios from 'axios';
 // const API_URL = 'http://3.91.40.230:8000/api';
-const API_URL = 'http://3.91.40.230:8000/api';
+const API_URL = 'http://localhost:8000/api';
 
 // Authentication functions
 export const register = async (username, email, password, phone_number) => {
